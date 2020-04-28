319cbd23a03e8d613427f1478df2ba7093b6b5e3
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2020-04-28 18:08:32 +0300

    Just example

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-const SocksAgent = require('socks5-https-client/lib/Agent');
-const { Telegraf } = require('telegraf');
 const fs = require('fs').promises;
+const { Telegraf } = require('telegraf');
+
+const SocksAgent = require('socks5-https-client/lib/Agent');
+const TelegrafInlineMenu = require('telegraf-inline-menu');
 
 const TOKEN_FILE = 'Tokenfile';
 const PROXY_FILE = 'Proxyfile';
+
+const runApp = (bot) => {
+  const menu = new TelegrafInlineMenu(
+    (ctx) => `Привет, ${ctx.from.first_name}!`
+  );
+
+  menu.setCommand('question');
+
+  menu.simpleButton('Просто кнопка', 'simple-button', {
+    doFunc: (ctx) => ctx.reply('Просто ответ, лол'),
+  });
+
+  menu.question('Задать вопрос', 'ask', {
+    uniqueIdentifier: 'ask',
+    questionText: 'Что ты хочешь спросить?',
+    setFunc: (_ctx, key) => {
+      _ctx.reply(`Твой вопрос: ${key}, мой ответ: нет.`);
+    },
+  });
+
+  bot.start((ctx) => ctx.reply('Привет!'));
+  bot.help((ctx) => ctx.reply('Отправь мне стикер'));
+  bot.on('sticker', (ctx) => ctx.reply('👍'));
+  bot.hears('hi', (ctx) => ctx.reply('Ну привет'));
+
+  bot.use(menu.init());
+  bot.startPolling();
+  bot.launch();
+};
 
 const main = async () => {
   const token = await fs.readFile(TOKEN_FILE, 'utf-8');
@@ -16,14 +47,7 @@ const main = async () => {
       },
     };
 
-    const bot = new Telegraf(token, botOptions);
-
-    bot.start((ctx) => ctx.reply('Welcome!'));
-    bot.help((ctx) => ctx.reply('Send me a sticker'));
-    bot.on('sticker', (ctx) => ctx.reply('👍'));
-    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-
-    bot.launch();
+    runApp(new Telegraf(token, botOptions));
     console.log('[BOT] Initialized.');
   } catch (err) {
     console.error(err);
